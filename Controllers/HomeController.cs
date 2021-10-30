682b8b75d94e67cc5adfaffd012551bff38e17ed
Fahim Faisal <fahimfaisal1998@gmail.com>
2021-10-30 22:42:00 +0600

    Post View Started

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BuddyHub.Repo;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,21 +11,8 @@ namespace BuddyHub.Controllers
     {
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            PostRepository pr = new PostRepository();
+            return View(pr.GetPostData());
         }
     }
 }
