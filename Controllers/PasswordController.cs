7088d2b9dd4dacf9d7b202df133dee7de230a252
Jahidul Islam Noor <jahidulislamananto@gmail.com>
2021-10-31 03:25:51 +0600

    Set recovery password done

---
+++ Controllers/PasswordController.cs
@@ -0,0 +1,36 @@
+﻿using BuddyHub.Models.VirtualModel;
+using BuddyHub.Repo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BuddyHub.Controllers
+{
+    public class PasswordController : Controller
+    {
+        // GET: Password
+        [HttpGet]
+        [Authorize]
+        public ActionResult SetRecoveryPassword()
+        {
+            int FK_Users_Id = (int)Session["UserId"];
+            RecoveryPasswordData rpd = new RecoveryPasswordData();
+            rpd = RecoveryPasswordRepository.FindUser(FK_Users_Id);
+            return View(rpd);
+        }
+
+        [HttpPost]
+        [Authorize]
+        public ActionResult SetRecoveryPassword(RecoveryPasswordData rpd)
+        {
+            if (ModelState.IsValid)
+            {
+                int FK_Users_Id = (int)Session["UserId"];
+                RecoveryPasswordRepository.SetQuestionAnswer(rpd, FK_Users_Id);
+            }
+            return View(rpd);
+        }
+    }
+}
--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -29,7 +29,8 @@ namespace BuddyHub.Controllers
                 if (user != null)
                 {
                     FormsAuthentication.SetAuthCookie(user.Username.ToString(), false);
-                    return RedirectToAction("Index", "Home");
+                    Session["UserId"] = user.Id; 
+                    return RedirectToAction("SetRecoveryPassword", "Password");
                 }
             }
              return View(ld);
