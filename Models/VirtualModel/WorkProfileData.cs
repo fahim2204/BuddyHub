9a71b10008db2c5932ec9cfdddce3bab760cd7e1
Jahidul Islam Noor <jahidulislamananto@gmail.com>
2021-11-02 06:50:35 +0600

    WorkProfileDone

---
+++ Models/VirtualModel/WorkProfileData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BuddyHub.Models.VirtualModel
+{
+    public class WorkProfileData
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Institution { get; set; }
+        public Nullable<System.DateTime> StartYear { get; set; }
+        public Nullable<System.DateTime> EndYear { get; set; }
+        [Required]
+        public string Position { get; set; }
+        public Nullable<int> FK_Users_Id { get; set; }
+    }
+}
