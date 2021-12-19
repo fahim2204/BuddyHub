e9e0c15a4b565668de0ade4321f8c8db373a9af3
19-39295-1@student.aiub.edu <19-39295-1@student.aiub.edu>
2021-12-19 22:57:57 +0600

    profile update

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header'
 import Login from './components/Login'
 import Register from './components/Register';
 import Home from './components/Home';
+import ProfileCard from './components/ProfileCard';
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/profile"  element={<ProfileCard/>}/>
       </Routes>
     </>
   );
