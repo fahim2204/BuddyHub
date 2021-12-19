89dc5b7da8ec90bbd59749129812c67cd16e8699
Fahim Faisal <fahimfaisal1998@gmail.com>
2021-12-19 19:58:51 +0600

    Starting Header

--- Frontend/src/components/Header.js
+++ Frontend/src/components/Header.js
@@ -2,36 +2,50 @@ import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import NavBar from './NavBar';
 import { isLoggedIn } from '../Config';
+import userImg from '../images/user.png';
+import '../App.css';
 
 const Header = () => {
     const navigate = useNavigate();
 
     const logout = () => {
-        console.log(sessionStorage.getItem('LoggedUser'));        
+        console.log(sessionStorage.getItem('LoggedUser'));
         sessionStorage.clear();
         navigate("/login");
     }
-    
 
+    const notification = () => {
+        alert("You are logged out!");
+    }
 
     return (
         <>
-            <nav className="navbar navbar-light bg-light fixed-top">
+            <nav className="navbar fixed-top">
                 <div className="container d-flex flex-nowrap shadow-sm">
-                    <div className="row w-100 align-items-center">
+                    <div className="row w-100 d-flex align-items-center">
                         <div className="col-4 d-flex justify-content-start">
                             <Link className="text-decoration-none text-secondary" to="/">
-                                <img className="img-fluid img-responsive"
+                                <img className="img-fluid img-responsive mouse-hover"
                                     src="/Buddyhub-logo.png" alt="Logo" width="170" height="100" />
                             </Link>
                         </div>
+                        <div className="col-4 d-flex justify-content-center"></div>
+                        <div className="col-4 d-flex justify-content-end">
+                            {isLoggedIn() && <>
+                                <div className="d-flex align-items-center">
+                                    <span className='me-2 border border-info rounded-circle mouse-hover'><img src={userImg} alt="user" width="35" height="35" className='rounded-circle m-0 p-0' /></span>
+                                    <span className='me-4 fw-bold'>{sessionStorage.getItem('Name')}</span>
+                                </div>
+
+                                <span className='text-success px-1 me-2 ms-4' onClick={notification}><i className="far fs-4 fa-bell mouse-hover"></i></span>
+                                <span className='text-primary px-1 py-0' onClick={logout}><i className="fas fs-4 fa-sign-out-alt mouse-hover"></i></span>
+                            </>}
+                            {!isLoggedIn() && <>
+                                <NavBar />
+                            </>}
+                        </div>
                     </div>
-                    {isLoggedIn() && <>
-                        <button className='btn btn-sm btn-success' onClick={logout}>Logout</button> 
-                    </>}
-                    {!isLoggedIn() && <>
-                        <NavBar />
-                    </>}                    
+
                 </div>
             </nav>
 
--- Frontend/src/components/Home.js
+++ Frontend/src/components/Home.js
@@ -1,25 +1,51 @@
 import React, { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { isLoggedIn } from '../Config';
+import { useAlert } from 'react-alert';
+
 
 function Home() {
 
-    useEffect(() => {document.title = "BuddyHub - Home"},[])
     const navigate = useNavigate();
+    const alert = useAlert();
 
-    // const isLoggedIn = us
+    useEffect(() => {
+        document.title = "BuddyHub - Home";
+        console.log(isLoggedIn());
+        if (!isLoggedIn()) {
+            navigate("/login");
+            alert.error("Please Login First!");
+        }
+    }, [navigate, alert])
 
-
-    
-if(sessionStorage.getItem('Token')){
-    return navigate('/login');
-}else{
 
     return (
-        <div>
-            <h5>This is Home</h5>
-        </div>
+        <>
+            {isLoggedIn() && <>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-8">
+                            <div className="card">
+                                <div className="card-body">
+                                    <h5 className="card-title">Welcome to BuddyHub</h5>
+                                    <p className="card-text">
+                                        This is a platform for sharing your thoughts and experiences with others.
+                                        You can share your thoughts and experiences with others.
+                                        You can also share your experiences with others.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-4">
+                            side Menu
+                        </div>
+                    </div>
+                </div>
+            </>}
+        </>
+
     )
-}
+
 
 
 }
