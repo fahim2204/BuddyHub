e8e4265b6d209893beb560573b0fe2fee5dc87e5
Jahidul Islam Noor <jahidulislamananto@gmail.com>
2021-12-20 19:25:26 +0600

    Not completed

--- Backend/BOL/Dto/ProfileDto.cs
+++ Backend/BOL/Dto/ProfileDto.cs
@@ -13,6 +13,8 @@ namespace BOL.Dto
         public int FK_Users_Id { get; set; }
         public string Username { get; set; }
         public string Name { get; set; }
+        public string Type { get; set; }
+        public int Status { get; set; }
         public string Contact { get; set; }
         public string Email { get; set; }
         public string ProfileImage { get; set; }
